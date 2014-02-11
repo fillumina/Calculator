d5c32835e926f52c6422ec76dea60dbfc3fe094a
Francesco Illuminati <fillumina@gmail.com>
2014-02-11 23:39:17 +0100

    fixed: Grammar so that different (but compatible) grammars can be joined

--- src/main/java/com/fillumina/utils/interpreter/Grammar.java
+++ src/main/java/com/fillumina/utils/interpreter/Grammar.java
@@ -1,6 +1,5 @@
 package com.fillumina.utils.interpreter;
 
-import com.fillumina.utils.interpreter.util.ImmutableIterator;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,30 +29,50 @@ public class Grammar<T,C>
         implements Iterable<GrammarElement<T,C>>, Serializable {
     private static final long serialVersionUID = 1L;
 
-    private final List<GrammarElement<T,C>> elements;
+    private final List<GrammarElement<? extends T,? extends C>> elements;
 
-    public Grammar(final GrammarElement<T,C>... elementsArray) {
+    public Grammar(final GrammarElement<? extends T,? extends C>... elementsArray) {
         elements = Collections.unmodifiableList(
                 Arrays.asList(elementsArray.clone()));
     }
 
-    public Grammar(final Collection<GrammarElement<T,C>> collection) {
+    public Grammar(final Collection<GrammarElement<? extends T,? extends C>> collection) {
         elements = Collections.unmodifiableList(new ArrayList<>(collection));
     }
 
     /** Always returns an immutable {@link Iterator}. */
     @Override
     public Iterator<GrammarElement<T, C>> iterator() {
-        return new ImmutableIterator<>(elements.iterator());
+        return new Iterator<GrammarElement<T, C>>() {
+            private final Iterator<GrammarElement<? extends T, ? extends C>>
+                    iterator = elements.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public GrammarElement<T, C> next() {
+                return (GrammarElement<T, C>) iterator.next();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("Removal not supported.");
+            }
+        };
     }
 
     /**
      * Creates a <b>new</b> {@link Grammar} with the new elements added, in the
      * given order, having the present elements first.
      */
-    public Grammar<T,C> join(final Grammar<T,C>... grammars) {
-        final List<GrammarElement<T,C>> list = new ArrayList<>(elements);
-        for (final Grammar<T,C> g: grammars) {
+    public Grammar<T,C> join(final Grammar<? extends T,? extends C>... grammars) {
+        final List<GrammarElement<? extends T,? extends C>> list =
+                new ArrayList<>(elements);
+        for (final Grammar<? extends T,? extends C> g: grammars) {
             list.addAll(g.elements);
         }
         return new Grammar<>(list);
